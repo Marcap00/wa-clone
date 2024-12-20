9decaf2286d7d88b0671a97bc58bf7bf413707db
Marcap00 <cappellatomarco@yahoo.com>
2024-12-20 14:24:47 +0100

    Rename project to "wa-clone-be" and add user profile fields: phone number, avatar, and biography. Implement migration to update users table accordingly

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,9 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'phone_number' => ['required', 'string', 'min:9', 'unique:users'],
+            'avatar' => ['nullable', 'text'],
+            'biography' => ['nullable', 'string'],
         ]);
     }
 
@@ -67,6 +70,9 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'phone_number' => $data['phone_number'],
+            'avatar' => $data['avatar'],
+            'biography' => $data['biography'],
         ]);
     }
 }
---
+++ database/migrations/2024_12_20_131429_add_other_columns_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone_number')->unique()->nullable(false)->after('password');
+            $table->string('biography')->nullable()->after('phone_number');
+            $table->text('avatar')->nullable()->after('biography');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('phone_number');
+            $table->dropColumn('biography');
+            $table->dropColumn('avatar');
+        });
+    }
+};
