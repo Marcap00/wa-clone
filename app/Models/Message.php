b8de31edf53e368550ab130c2390b6afb131fdaa
Marcap00 <cappellatomarco@yahoo.com>
2024-12-21 21:20:31 +0100

    Add contact relationship belongsTo to Message model

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Message extends Model
@@ -17,4 +18,9 @@ class Message extends Model
         'message',
         'status',
     ];
+
+    public function contact(): BelongsTo
+    {
+        return $this->belongsTo(Contact::class);
+    }
 }
