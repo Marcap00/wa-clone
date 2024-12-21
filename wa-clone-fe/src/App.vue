c5efa3b73e15a4a6c11586b410678ec140b7a705
Marcap00 <cappellatomarco@yahoo.com>
2024-12-22 00:38:45 +0100

    Initialize Vue 3

---
+++ wa-clone-fe/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <HelloWorld />
+</template>
+
+<style lang="scss">
+@use "bootstrap/scss/bootstrap.scss" as *;
+@use "./scss/general.scss" as *;
+</style>
