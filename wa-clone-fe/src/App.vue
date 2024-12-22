cba41adf07f16c7af0ee8e72f8f443a74fb80b2a
Marcap00 <cappellatomarco@yahoo.com>
2024-12-22 03:35:30 +0100

    Refactor App.vue to replace HelloWorld component with AppMain, and update SCSS imports to include bootstrap-grid and bootstrap-utilities

--- wa-clone-fe/src/App.vue
+++ wa-clone-fe/src/App.vue
@@ -1,12 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import AppMain from './components/AppMain.vue'
 </script>
 
 <template>
-    <HelloWorld />
+    <AppMain />
 </template>
 
 <style lang="scss">
-@use "bootstrap/scss/bootstrap.scss" as *;
+// @use "bootstrap/scss/bootstrap.scss" as *;
+
+@use "bootstrap/scss/bootstrap-grid.scss" as *;
+@use "bootstrap/scss/bootstrap-utilities.scss" as *;
+
 @use "./scss/general.scss" as *;
 </style>
