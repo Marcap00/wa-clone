cc5fb3719c4a41f77e85b2d73560ceb4ac9d6be1
Marcap00 <cappellatomarco@yahoo.com>
2024-12-22 04:11:18 +0100

    Update AppMainChatInput component by adding a name attribute to the input field for new messages

--- wa-clone-fe/src/components/AppMainChatInput.vue
+++ wa-clone-fe/src/components/AppMainChatInput.vue
@@ -8,7 +8,7 @@ const newMessage = ref('');
     <div class="input-message d-flex  align-items-center p-2">
         <i class="far fa-smile fa-lg mx-3"></i>
 
-        <input v-model="newMessage" @keyup.enter="addMessage(newMessage, activeIndex)"
+        <input v-model="newMessage" name="newMessage" @keyup.enter="addMessage(newMessage, activeIndex)"
             class="flex-grow-1 border-0 rounded-3 ps-3" type="text" placeholder="Scrivi un messaggio">
         <i class="fas fa-microphone mx-3"></i>
     </div>
