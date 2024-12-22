324fbb24af7c1831688c0a2cf72ec627dfc9bc56
Marcap00 <cappellatomarco@yahoo.com>
2024-12-22 03:51:58 +0100

    Add input field for new messages in AppMain component

--- wa-clone-fe/src/components/AppMain.vue
+++ wa-clone-fe/src/components/AppMain.vue
@@ -86,6 +86,13 @@ onMounted(async () => {
                             </li>
                         </ul>
                     </div>
+                    <div class="input-message d-flex  align-items-center p-2">
+                        <i class="far fa-smile fa-lg mx-3"></i>
+
+                        <input v-model="newMessage" @keyup.enter="addMessage(newMessage, activeIndex)"
+                            class="flex-grow-1 border-0 rounded-3 ps-3" type="text" placeholder="Scrivi un messaggio">
+                        <i class="fas fa-microphone mx-3"></i>
+                    </div>
                 </div>
             </div>
         </div>
