ecd6e38dd59f4f3eb226604d4df18c82796258d4
Marcap00 <cappellatomarco@yahoo.com>
2024-12-31 16:50:52 +0100

    Refactor AppFooter component: update layout, enhance styling, and add social media links

--- wa-clone-fe/src/components/general/AppFooter.vue
+++ wa-clone-fe/src/components/general/AppFooter.vue
@@ -4,17 +4,20 @@
 <template>
     <footer class="footer">
         <div class="footer-content d-flex justify-content-between align-items-center gap-3 h-100 p-3">
-            <div class="d-flex align-items-center gap-3">
-                <img src="/src/assets/img/whatsapp.png" alt="Logo" class="img-fluid">
-                <p>WA Clone</p>
-            </div>
             <div>
-                <p>Created by Marco Cappellato</p>
+                <div class="d-flex align-items-center gap-3">
+                    <img src="/src/assets/img/whatsapp.png" alt="Logo" class="img-fluid">
+                    <address>Created by Marco Cappellato</address>
+                </div>
             </div>
-            <address>
-                <i class="fa-brands fa-github"></i>
-                <i class="fa-brands fa-linkedin"></i>
-            </address>
+            <div class="d-flex align-items-center gap-3">
+                <a href="https://github.com/Marcap00" target="_blank">
+                    <i class="fa-brands fa-github fa-2x"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/marco-cappellato-5b83a4322" target="_blank">
+                    <i class="fa-brands fa-linkedin fa-2x"></i>
+                </a>
+            </div>
         </div>
     </footer>
 </template>
@@ -29,6 +32,10 @@ footer {
 
     img {
         width: 30px;
+    }
+
+    i:hover {
+        color: $text-label-active;
     }
 }
 </style>
