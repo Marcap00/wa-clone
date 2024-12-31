8ba21ddd418e90b99f07eec9c79b0a298f8d3bee
Marcap00 <cappellatomarco@yahoo.com>
2024-12-31 18:49:35 +0100

    Remove Header and Footer

--- wa-clone-fe/src/pages/AppHome.vue
+++ wa-clone-fe/src/pages/AppHome.vue
@@ -1,13 +1,9 @@
 <script setup>
 import AppHomeMain from '../components/home/AppHomeMain.vue'
-import AppHeader from '../components/general/AppHeader.vue'
-import AppFooter from '../components/general/AppFooter.vue'
 </script>
 
 <template>
-    <AppHeader />
     <main class="h-main">
         <AppHomeMain />
     </main>
-    <AppFooter />
 </template>
