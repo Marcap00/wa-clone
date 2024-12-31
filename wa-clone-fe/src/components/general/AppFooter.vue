72d8b2ab878cc0cbb02cf8be1075ffe543a6e733
Marcap00 <cappellatomarco@yahoo.com>
2024-12-31 01:28:04 +0100

    Enhance AppFooter component with new layout and styling.

--- wa-clone-fe/src/components/general/AppFooter.vue
+++ wa-clone-fe/src/components/general/AppFooter.vue
@@ -2,14 +2,33 @@
 </script>
 
 <template>
-    <footer>
-        <p>Footer</p>
+    <footer class="footer">
+        <div class="footer-content d-flex justify-content-between align-items-center gap-3 h-100 p-3">
+            <div class="d-flex align-items-center gap-3">
+                <img src="/src/assets/img/whatsapp.png" alt="Logo" class="img-fluid">
+                <p>WA Clone</p>
+            </div>
+            <div>
+                <p>Created by Marco Cappellato</p>
+            </div>
+            <address>
+                <i class="fa-brands fa-github"></i>
+                <i class="fa-brands fa-linkedin"></i>
+            </address>
+        </div>
     </footer>
 </template>
 
 <style lang="scss" scoped>
+@import '../../scss/_variables.scss';
+
 footer {
     height: 70px;
-    background-color: #eaeaea;
+    background-color: $bg-dark-contacts;
+    color: $text-title;
+
+    img {
+        width: 30px;
+    }
 }
 </style>
