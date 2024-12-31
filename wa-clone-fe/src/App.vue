ef0d4c54e9d4a155e72b2be4899718f32dd01d56
Marcap00 <cappellatomarco@yahoo.com>
2024-12-31 18:50:12 +0100

    Add AppFooter and AppHeader and added control route path for appearing and disappering

--- wa-clone-fe/src/App.vue
+++ wa-clone-fe/src/App.vue
@@ -1,9 +1,16 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router'
+import AppHeader from './components/general/AppHeader.vue'
+import AppFooter from './components/general/AppFooter.vue'
+import { computed } from 'vue'
+const route = useRoute()
+const isDashboardPage = computed(() => route.path.includes('dashboard'))
 </script>
 
 <template>
+    <AppHeader v-if="!isDashboardPage" />
     <RouterView />
+    <AppFooter v-if="!isDashboardPage" />
 </template>
 
 <style lang="scss">
