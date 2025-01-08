62797de122a08b97ea75e6f29c2edbe2e600d762
Marcap00 <cappellatomarco@yahoo.com>
2025-01-08 15:13:22 +0100

    Enhance AppMainChatInput component with auto-reply functionality

--- wa-clone-fe/src/components/chat/AppMainChatInput.vue
+++ wa-clone-fe/src/components/chat/AppMainChatInput.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { useContactsStore } from '../../js/stores/contacts';
 import { useActiveIndexStore } from '../../js/stores/activeIndex';
 import { useRouter } from 'vue-router';
@@ -11,8 +11,25 @@ const activeIndexStore = useActiveIndexStore()
 const newMessage = ref('');
 const ApiUrl = 'http://localhost:8000/api/messages/store';
 
+const activeIndex = computed(() => activeIndexStore.activeIndex)
+
 function getContacts() {
     contactsStore.getContacts(router);
+}
+
+// Autoreply message
+async function autoReplyMessage(index) {
+    try {
+        await axios.post(ApiUrl, {
+            message: 'Hello!',
+            date: null,
+            status: 'received',
+            contact_id: contactsStore.contacts[index].id,
+        })
+        getContacts()
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 async function addMessage(message, index) {
@@ -23,8 +40,11 @@ async function addMessage(message, index) {
             status: 'sent',
             contact_id: contactsStore.contacts[index].id,
         })
-        getContacts();
+        setTimeout(() => {
+            autoReplyMessage(index)
+        }, 2000);
         newMessage.value = '';
+        getContacts();
     } catch (error) {
         console.error(error);
     }
@@ -37,7 +57,7 @@ async function addMessage(message, index) {
 
         <!-- <form class="flex-grow-1"> -->
         <input v-model="newMessage" name="message" class="w-100 border-0 rounded-3 ps-3" type="text"
-            placeholder="Type a message" @keyup.enter="addMessage(newMessage, activeIndexStore.activeIndex)">
+            placeholder="Type a message" @keyup.enter="addMessage(newMessage, activeIndex)">
         <!-- </form> -->
 
         <i class="fas fa-microphone mx-3"></i>
