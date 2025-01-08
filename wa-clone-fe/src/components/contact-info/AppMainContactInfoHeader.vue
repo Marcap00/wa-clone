b0079ee9cd80e5216a140f5f6bc95a6115356357
Marcap00 <cappellatomarco@yahoo.com>
2025-01-08 13:30:19 +0100

    Implement AppMainContactInfoHeader component

---
+++ wa-clone-fe/src/components/contact-info/AppMainContactInfoHeader.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useContactInfoStore } from '../../js/stores/contactInfo';
+const contactInfoStore = useContactInfoStore();
+const closeContactInfo = () => {
+    contactInfoStore.contactInfo = false;
+};
+</script>
+
+<template>
+    <div class="header-info d-flex align-items-center gap-3 px-3 py-3">
+        <i class="fas fa-xmark fa-lg" @click="closeContactInfo"></i>
+        <h3>Contact Info</h3>
+    </div>
+</template>
+
+<style scoped></style>
